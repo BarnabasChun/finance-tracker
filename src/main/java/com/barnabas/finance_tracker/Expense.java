a846be59cc8f8c654d8e7f1f289ac116359213f8
Barnabas Chun <28585999+BarnabasChun@users.noreply.github.com>
2026-03-01 11:28:11 -0500

    Implement Expense entity with JPA

---
+++ src/main/java/com/barnabas/finance_tracker/Expense.java
@@ -0,0 +1,27 @@
+package com.barnabas.finance_tracker;
+
+// Jakarta Persistence (JPA) allows for mapping Java objects to database tables using annotations and entity classes
+
+import jakarta.persistence.*; // if you use more than a certain number of classes from the same package (usually 5), editor automatically collapses them into a wildcard import to keep the top of your file "clean."
+import lombok.Data; // eliminated need to manually write Getters, Setters
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class Expense {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //    delegates ID assignment to the database using an auto-incremented column
+    private Long id;
+
+    private String description;
+    private Double amount;
+    private String category;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+}
