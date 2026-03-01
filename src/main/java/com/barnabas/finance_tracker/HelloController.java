b60bec4cb2c81a64335ed34b75e226a0d69a2be4
Barnabas Chun <28585999+BarnabasChun@users.noreply.github.com>
2026-03-01 11:27:16 -0500

    Add hello world controller and verify spring boot setup

---
+++ src/main/java/com/barnabas/finance_tracker/HelloController.java
@@ -0,0 +1,13 @@
+package com.barnabas.finance_tracker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Spring Boot 4 is alive!";
+    }
+}
